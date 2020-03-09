67550303aa11b505aa33f43f1ffde9bee3022d98
enl001 <enliven001@mail.ru>
2020-03-09 17:20:04 +0300

    feat: basic algorithm

--- src/index.js
+++ src/index.js
@@ -4,9 +4,95 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let operations = [];
+    let values = [];
+    expr = expr.replace(/' '/g,'');
+    for(let e of expr){
+        switch (e) {
+
+        }
+    }
+
 }
 
 module.exports = {
     expressionCalculator
 }
+
+function expressionCalculator(expr) {
+    let operations = [];
+    let values = [];
+    console.log(expr);
+    let exprToCalc = expr.replace(/\s+/g,'');
+
+
+    console.log(exprToCalc);
+    let value = '';
+    for(let e of exprToCalc){
+      
+        switch (e) {          
+          case ')':
+            if (value.length>0) {
+              values.push(parseFloat(value));
+              value = '';
+            }           
+            values.push(calculate(operations, values));            
+          break;
+          case '(':            
+            //value = '';            
+          break;
+          case '+':
+            operations.push(e);
+            if (value.length>0) {
+              values.push(parseFloat(value));
+              value = '';
+            }
+          break;
+          case '-':
+            operations.push(e);
+            if (value.length>0) {
+              values.push(parseFloat(value));
+              value = '';
+            }
+          break;
+          case '/':
+            operations.push(e);
+            if (value.length>0) {
+              values.push(parseFloat(value));
+              value = '';
+            }
+          break;
+          case '*':
+            operations.push(e);
+            if (value.length>0) {
+              values.push(parseFloat(value));
+              value = '';
+            }
+          break;
+          default:
+            value+=e;
+          break
+        }       
+    }
+    return values.pop();
+}
+
+function calculate(operations,values){  
+  let value = parseFloat(values.pop());
+  switch (operations.pop()){
+    case '+':
+      value = parseFloat(values.pop()) + value;
+    break;
+    case '-':
+      value = parseFloat(values.pop()) - value;
+    break;
+    case '/':
+      value = parseFloat(values.pop()) / value;
+    break;
+    case '*':
+      value = parseFloat(values.pop()) * value;
+    break;    
+  }  
+  return value;
+}
+console.log(expressionCalculator("   100 - 60 / 38)) +   19 / 88) * 97) / 82 )/ 94  ) * 92 )) "));
